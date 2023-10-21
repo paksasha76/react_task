cd5646306a21a0d897d7a4f76ac27c9a393d37ca
paksasha76 <pak.aleksandr2001@mail.ru>
2023-10-22 01:58:18 +0300

    project

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,12 +3,14 @@ import { HeaderCurrency } from "./HeaderCurrency/HeaderCurrency";
 import { HeaderImg } from "./HeaderImg/HeaderImg";
 
 import "./Header.css";
+import {HeaderSelect} from "./HeaderSelect/HeaderSelect"
 
 export function Header() {
   return (
     <header className="header">
       <div className="header__left">
         <HeaderContainer />
+        <HeaderSelect />
       </div>
       <HeaderImg />
     </header>
---
+++ src/Components/Header/HeaderSelect/HeaderSelect.tsx
@@ -0,0 +1,13 @@
+export function HeaderSelect() {
+    return(
+        <div className="header__select">
+            <div className="header__select-btn">RUB</div>
+            <div className="header__select-content">
+                <div className="header__select-item">AED</div>
+                <div className="header__select-item">RUB</div>
+                <div className="header__select-item">ALL</div>
+                <div className="header__select-item">AMD</div>
+            </div>
+        </div>
+    )
+}
