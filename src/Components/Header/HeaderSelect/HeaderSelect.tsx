2fd1a1ae1c57dc16ee03384a7f50f9f2d144e4cb
paksasha76 <pak.aleksandr2001@mail.ru>
2023-10-22 03:30:18 +0300

    project

--- src/Components/Header/HeaderSelect/HeaderSelect.tsx
+++ src/Components/Header/HeaderSelect/HeaderSelect.tsx
@@ -1,13 +1,21 @@
+import React from 'react'
+
 export function HeaderSelect() {
+    const [isActive, setIsActive] = React.useState(false)
+    function openSelect() {
+        setIsActive(true)
+    }
     return(
         <div className="header__select">
-            <div className="header__select-btn">RUB</div>
+            <div className="header__select-btn" onClick={(e) => setIsActive(!isActive)}>RUB</div>
+            { isActive &&
             <div className="header__select-content">
                 <div className="header__select-item">AED</div>
                 <div className="header__select-item">RUB</div>
                 <div className="header__select-item">ALL</div>
                 <div className="header__select-item">AMD</div>
             </div>
+}
         </div>
     )
 }
