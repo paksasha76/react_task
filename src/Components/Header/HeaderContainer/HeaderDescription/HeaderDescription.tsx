254aea093580433d682b1dc8e620e0a44a89de22
paksasha76 <pak.aleksandr2001@mail.ru>
2023-10-23 11:28:36 +0300

    project

--- src/Components/Header/HeaderContainer/HeaderDescription/HeaderDescription.tsx
+++
@@ -1,9 +0,0 @@
-import "./HeaderDescription.css"
-
-export function HeaderDescription() {
-  return (
-    <div>
-      <p className="header__description">currencies academic terms</p>
-    </div>
-  );
-}
